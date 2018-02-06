cfeef3deffe45b71dbf952abff73d7415374018b
GorodetskiyKP <gorodetskiykp@gmail.com>
2018-02-06 15:43:11 +1000

    Add PyQt5 script for getting unique words

---
+++ unique_words_qt/main.py
@@ -0,0 +1,56 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QDesktopWidget, QMainWindow, QToolTip, QPushButton, QApplication, QHBoxLayout, QVBoxLayout, QTextEdit, QGridLayout)
+from PyQt5.QtGui import QFont, QIcon
+
+class MainApplication(QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        QToolTip.setFont(QFont("SansSerif", 10))
+
+        btn = QPushButton("Get unique words", self)
+        btn.clicked.connect(self.buttonClicked)
+
+        self.full_text = QTextEdit()
+        self.unique_text = QTextEdit()
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(self.full_text, 1, 1)
+        grid.addWidget(self.unique_text, 1, 2)
+        grid.addWidget(btn, 2, 2)
+
+        self.setLayout(grid)
+
+        self.resize(500, 500)
+        self.center()
+        self.setWindowTitle("Unique words")
+
+        self.show()
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def buttonClicked(self):
+
+        text = self.full_text.toPlainText()
+        text_list = str(text).lower().split()
+        text = sorted(set(text_list))
+        self.unique_text.setText('\n'.join(text))
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    window = MainApplication()
+    sys.exit(app.exec_())
